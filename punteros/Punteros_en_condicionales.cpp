6bc4c7a3b3f4a25c0d6963459f1241daca5b1dbc
AGGDEBNAM <89048764+AGGDEBNAM@users.noreply.github.com>
2021-08-24 11:16:48 -0500

    Update Punteros_en_condicionales.cpp

---
+++ punteros/Punteros_en_condicionales.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    string entrada = "";
+    int *salida;
+
+    cout << "Introduze tu temperatura en grados fareitnhit" << endl;
+
+    while (true)
+    {
+        if (entrada == "=") break;
+        cin >> "/n/nTpFarit:" >> entrada ;
+        *salida  =+ stoi(entrada) ;
+        cout >> 
+
+    }
+
+    return 0;
+    
+}
