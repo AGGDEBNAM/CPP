546f344ed18b75387f5013f363d1f0c61949165d
AGGDEBNAM <89048764+AGGDEBNAM@users.noreply.github.com>
2021-08-25 09:35:16 -0500

    Update temperatura.cpp

--- Pruebas-Practicas/temperatura.cpp
+++ Pruebas-Practicas/temperatura.cpp
@@ -7,8 +7,9 @@ using namespace std;
 int main()
 {
 
-    string entrada;
-    int guardado;
+    string entrada [1];
+    int guardado1;
+    int guardado2;
     int division = 5/9;
 
     while (true)
@@ -17,31 +18,29 @@ int main()
         
         cout << "  FAHRENHEIT A CELSIUS " << endl;
         cout << "  CELSIUS A FAHRENHEIT " << endl;
-        cout << "  \n\n\n " << endl;
         cout << "  Ingresa ' F ' para calcular celsius " << endl;
         cout << "  Ingresa ' C ' para calcular fahrenheit " << endl;
         cout << "  Ingresa ' S ' para salir " << endl;
         cin >> entrada ;
 
+        if(entrada == "S" ) break;
+
         if(entrada == "F" );
         {
 
-            cin >> guardado ;
-            cout << " Introduze temperatura en fahrenheit " << division * stoi(guardado-32);
-            cout >> guardado >> endl;
-            fflush(guardado);
+            cin >> guardado1 ;
+            cout << " Introduze temperatura en fahrenheit " << division * guardado1 - 32 << endl;
+            cout << guardado1 << endl;
 
         }
         if(entrada == "C" );
         {
 
-            cin >> guardado ;
-            cout << " Introduze temperatura en fahrenheit " << division * stoi(guardado+32);
-            cout >> guardado >> endl;
-            fflush(guardado);
+            cin >> guardado2 ;
+            cout << " Introduze temperatura en fahrenheit " << division * guardado2 + 32 << endl;
+            cout << guardado2 << endl;
 
         }
-        if(entrada == "S" ) break;
 
     }
     
