3539ffbe9d7249b629ae668eccee7dfad5b1aa88
AGGDEBNAM <a20100306@ceti.mx>
2021-10-06 09:52:16 -0500

    VSC

---
+++ poo_clase/pr1_p14D_20100306.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class Persona{
+    public:
+        char nombre[50];
+        int edad;
+        char domicilio[60];
+        //muestra
+        pedro , juan;
+}
+
+int main()
+{
+
+    cout << "introduzca los valores de pedro: ";
+    gets(Pedro.nombre);
+    cin >> Pedro.edad;
+    gets(Pedro.domicilio);
+    cout << "introduzca los valores de Juan: ";
+    gets(Juan.nombre);
+    cin >> Juan.edad;
+    gets(Juan.domicilio);
+
+    return 0;
+
+}
